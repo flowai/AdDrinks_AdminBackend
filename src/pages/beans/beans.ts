5f2af6a291460c22eb5179d3a42e1948827f71c4
flowain <f.waidner@gmx.de>
2018-01-21 02:04:58 +0100

    added Pics and delete Button as well as add functionality

--- src/pages/beans/beans.ts
+++ src/pages/beans/beans.ts
@@ -2,10 +2,12 @@ import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
 import { AngularFirestore, AngularFirestoreCollection, AngularFirestoreDocument} from 'angularfire2/firestore';
 import { Observable } from 'rxjs/Observable';
+import { AlertController } from 'ionic-angular';
 
 interface Bean {
   title: string;
   value: number;
+  src: string;
 }
 
 @Component({
@@ -17,10 +19,54 @@ export class BeansSite {
   beansCol: AngularFirestoreCollection<Bean>;
   beans: Observable<Bean[]>;
 
-  constructor(public navCtrl: NavController, private afs: AngularFirestore) {
+  title: string;
+  value: number;
+
+  constructor(public navCtrl: NavController, private afs: AngularFirestore, public alertCtrl: AlertController) {
     this.beansCol = this.afs.collection<Bean>('beans');
     this.beans = this.beansCol.valueChanges();
     //const groceryListRef = this.fireStore.collection<Grocery>(`/groceryList`);
+  }
+
+  addEntry(event) {
+    let alert = this.alertCtrl.create({
+      title: 'Neuer Eintrag (Bohnen)',
+      inputs: [{
+        name: 'Sorte',
+        placeholder: 'Sorte'
+      },{
+        name: 'Anzahl',
+        placeholder: 'Anzahl'
+      },{
+        name: 'Link',
+        placeholder: 'https://upload.wikimedia.org/wikipedia/commons/thumb/4/45/A_small_cup_of_coffee.JPG/275px-A_small_cup_of_coffee.JPG'
+      }],
+      buttons: [
+        {
+          text: 'Abbrechen',
+          handler: data => {
+            console.log('Cancel new Bean');
+          }
+        },
+        {
+          text: 'Speichern',
+          handler: data => {
+            this.addPost(data.Sorte, data.Anzahl, data.Link);
+            console.log('Saved Bean');
+          }
+        }
+      ]
+    });
+    alert.present();
+  }
+
+  addPost(title: string, value: number, src: string) {
+    this.beansCol.add({'title': title, 'value': value, 'src': src});
+    console.log('Saved Bean in Firestore: ' + title);
+  }
+
+  deleteEntry() {
+    
   }
 
 }
